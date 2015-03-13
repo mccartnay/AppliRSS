1c448731f64b1f51995e006bcd686e020cf93fdd
mccartnay <erwan.marchand@orange.fr>
2015-03-13 16:06:37 +0100

    Ajout des méthodes et attributs

--- src/controleur/Gestionnaire.java
+++ src/controleur/Gestionnaire.java
@@ -2,4 +2,76 @@ package controleur;
 
 public class Gestionnaire {
 
+	private ArrayList<Site> sites;
+	private RSSAgent agent;
+
+	public Gestionnaire(RSSAgent unAgent) {
+
+		sites = new ArrayListe();
+		agent = unAgant;
+
+	}
+
+	//Getter and Setter
+
+	public Agent getAgent() {
+
+		return agent;
+
+	}
+
+	public void setAgent(RSSAgent unAgent) {
+
+		agent = unAgent;
+
+	}
+
+	public void AfficherTout() {
+
+		//void
+
+	}
+
+	public void AfficherFavoris() {
+
+		//void
+
+	}
+
+	public void ajoutFavoris(Site unSite) {
+
+		unSite.setFavoris(true);
+
+	}
+
+	public void supprimerFavoris(Site unSite) {
+
+		unSite.setFavoris(false);
+
+	}
+
+	public void ajouterSite(Site unSite) {
+
+		sites.add(unSite);
+
+	}
+
+	public void supprimerSite(Site unSite) {
+
+		sites.remove(unSite);
+
+	}
+
+	public void majNews() {
+
+		//void
+
+	}
+
+	public void trierSite() {
+
+		//void
+
+	}
+
 }

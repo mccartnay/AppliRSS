fa35dd75681579f47c38415b3ca3d9752e3a5edb
mccartnay <erwan.marchand@orange.fr>
2015-03-17 11:44:05 +0100

    Implémentation de la structure de la classe Moteur

--- src/controleur/Moteur.java
+++ src/controleur/Moteur.java
@@ -2,4 +2,21 @@ package controleur;
 
 public class Moteur {
 
+	private Gestionnaire gestionnaire;
+	private MoteurUI moteurUI;
+
+	public Moteur(Gestionnaire unGestionnaire, MoteurUI unMoteurUI) {
+
+		gestionnaire = unGestionnaire;
+		moteurUi = unMoteurUI;
+
+	}
+
+	public static void main() {
+
+
+
+	}
+
+
 }

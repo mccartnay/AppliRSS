4be444928e35b7e9daf6c65d159dde12950a6557
mccartnay <erwan.marchand@orange.fr>
2015-03-17 12:45:32 +0100

    Création de JPanelSites.java

---
+++ src/vue/JPanelSites.java
@@ -0,0 +1,46 @@
+/*
+ * JPanelSites.java
+ *
+ * Copyright (C) 2015 Mathieu Gaborit (matael) <mathieu@matael.org>
+ *
+ *
+ * Distributed under WTFPL terms
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
+
+public class JPanelSites {
+
+	private JButton bQuitter;
+	private MoteurUI moteurUI;
+
+	public JPanelSites(MoteurUI unMoteurUI) {
+
+		moteurUI = unMoteurUI;
+		bQuitter = new JButton("Quitter");
+
+		this.add(bQuitter, BorderLayout.SOUTH);
+		bQuitter.addActionListener(new ActionListener(){
+			public void ActionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+
+	}
+
+}
+
+

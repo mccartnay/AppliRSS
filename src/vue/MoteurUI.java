c7cab1f1dc118e45388bb18c500da5641d9ccfd2
mccartnay <erwan.marchand@orange.fr>
2015-03-17 11:53:43 +0100

    Ajout de MoteurUI.hava

---
+++ src/vue/MoteurUI.java
@@ -0,0 +1,60 @@
+/*
+ * MoteurUI.java
+ *
+ * Copyright (C) 2015 Erwan Marchand (mccartnay) <erwan.marchand@orange.fr>
+ *
+ *
+ * Distributed under WTFPL terms
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
+
+public class MoteurUI {
+
+	private JFrameMenu JFM;
+	private JFrameNews JFN;
+	private JFrameSites JFS;
+
+	public MoteurUI(JFrameMenu uneJFM, JFrameNews uneJFN, JFrameSites uneJFS) {
+
+		JFM = uneJFM;
+		JFN = uneJFN;
+		JFS = uneJFS;
+
+	}
+
+
+	public void paint() {
+
+
+
+	}
+
+	public void repaint() {
+
+
+
+	}
+
+	public void actualiser() {
+
+
+
+	}
+
+}
+
+

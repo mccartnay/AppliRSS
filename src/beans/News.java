65dce62035c99f6bf9b6e81b4c55e8b90f2d61d8
mccartnay <erwan.marchand@orange.fr>
2015-03-20 18:19:49 +0100

    Suppresion de la date et de l'id

--- src/beans/News.java
+++ src/beans/News.java
@@ -3,41 +3,34 @@ package beans;
 import java.util.Date;
 
 public class News {
-	private int id;
 	private String titre;
-	private Date date;
 	private String texte;
 	private String urlNews;
 
 
-	public News(int unId, String unTitre, Date uneDate, String texte, String uneUrl) {
+	public News(String unTitre, String texte, String uneUrl) {
 
-		id = unId;
 		titre = unTritre;
-		date = uneDate;
 		texte = unTexte;
 		urlNews = uneUrl;
 
 	}
 
+	public News() {
+
+		titre = "";
+		texte = "";
+		urlNews = "";
+
+	}
+
+
 	//Getter and Setter
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
 	public String getTitre() {
 		return titre;
 	}
 	public void setTitre(String titre) {
 		this.titre = titre;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
 	}
 	public String getTexte() {
 		return texte;

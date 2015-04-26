0f33277d42129ed7f68f94902c83c5de8c58ef26
Nonghorn <niels.desparre@gmail.com>
2015-04-26 14:48:11 +0200

    Création d'une interface pour la mise en place d'un event lors de la mise à jour des news

---
+++ src/controleur/NewsUpdatedListener.java
@@ -0,0 +1,10 @@
+package controleur;
+
+/**
+ * Interface de mise en place d'un listener pour la mise à jour des news
+ * @author niels
+ *
+ */
+public interface NewsUpdatedListener {
+	public void newsUpdated();
+}
